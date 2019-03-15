7ae7d0693dc5cfcb2b9ddfebc80284d2c66e793b
Terkwood <metaterkhorn@gmail.com>
2019-03-14 21:58:33 -0400

    Draw a clock using js date
    Squashed commit of the following:
    
    commit 82be568147e0b33fa5d0cd4dd9ba977570a881c8
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Thu Mar 14 21:56:51 2019 -0400
    
        change the colors
    
    commit 20a44432c12f6dd600861f42581110e057919ecb
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Thu Mar 14 21:53:37 2019 -0400
    
        draw a clock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,86 @@
+// Draw a clock with changed draw and update rates and disabled vsync
+extern crate quicksilver;
+
+use stdweb::web::Date;
+
+use quicksilver::{
+    geom::{Circle, Line, Vector},
+    graphics::{Background::Col, Color},
+    lifecycle::{run, Settings, State, Window},
+    Result,
+};
+
+struct Clock {
+    hours: u32,
+    minutes: u32,
+    seconds: u32,
+}
+
+impl State for Clock {
+    fn new() -> Result<Clock> {
+        let now = Date::new();
+        Ok(Clock {
+            hours: now.get_hours() as u32,
+            minutes: now.get_minutes() as u32,
+            seconds: now.get_seconds() as u32,
+        })
+    }
+
+    fn update(&mut self, _window: &mut Window) -> Result<()> {
+        let now = Date::new();
+        self.hours = now.get_hours() as u32;
+        self.minutes = now.get_minutes() as u32;
+        self.seconds = now.get_seconds() as u32;
+        Ok(())
+    }
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        // clear everything
+        window.clear(Color::BLACK)?;
+
+        // draw the frame
+        window.draw(&Circle::new((400, 300), 203), Col(Color::WHITE));
+        window.draw(&Circle::new((400, 300), 200), Col(Color::BLACK));
+
+        // draw the hour markers
+        for i in 1..=12 {
+            let angle = 360. * ((i as f64 + 9.) * 2. / 24.);
+            let pos = Vector::from_angle(angle as f32) * 200. + Vector::new(400, 300);
+            let line = Line::new((400, 300), pos).with_thickness(5);
+            window.draw(&line, Col(Color::WHITE));
+        }
+
+        window.draw(&Circle::new((400, 300), 180), Col(Color::BLACK));
+
+        let hour_angle = 360. * ((self.hours as f64 + 9.) * 2. / 24.);
+        let minute_angle = 360. * ((self.minutes as f64 + 45.) / 60.);
+        let second_angle = 360. * ((self.seconds as f64 + 45.) / 60.);
+
+        let hour_pos = Vector::from_angle(hour_angle as f32) * 150. + Vector::new(400, 300);
+        let min_pos = Vector::from_angle(minute_angle as f32) * 180. + Vector::new(400, 300);
+        let second_pos = Vector::from_angle(second_angle as f32) * 180. + Vector::new(400, 300);
+
+        let hour = Line::new((400, 300), hour_pos).with_thickness(10);
+        let minute = Line::new((400, 300), min_pos).with_thickness(5);
+        let second = Line::new((400, 300), second_pos).with_thickness(3);
+
+        window.draw(&hour, Col(Color::WHITE));
+        window.draw(&minute, Col(Color::CYAN));
+        window.draw(&second, Col(Color::PURPLE));
+
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    run::<Clock>(
+        "Clock",
+        Vector::new(800, 600),
+        Settings {
+            draw_rate: 1000. / 10., // 10 FPS are enough
+            update_rate: 1000.,     // every second to make it appear like a clock
+            vsync: false,           // don't use VSync, we're limiting to 10 FPS on our own
+            ..Settings::default()
+        },
+    );
 }
